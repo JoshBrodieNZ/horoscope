391f0d4317b8ca5b5f04ba0a913ad085a9ba7f9f
JoshBrodieNZ <joshbrodienz@gmail.com>
2015-02-20 11:48:54 +0000

    first commit

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source 'https://rubygems.org/'
+
+gem 'marky_markov'
+gem 'sinatra'
---
+++ app.rb
@@ -0,0 +1,20 @@
+require 'sinatra'
+require 'marky_markov'
+
+set :bind, '0.0.0.0'
+set :port, 3000
+
+zodiacs = %w(aquarius aries cancer capricorn gemini leo libra pisces sagittarius scorpio taurus virgo)
+
+dictionaries = {}
+
+zodiacs.each do |zodiac|
+  dictionaries[zodiac] = MarkyMarkov::Dictionary.new("dictionaries/#{zodiac}")
+end
+
+get '/:zodiac' do
+  @zodiac = params[:zodiac]
+  @content = dictionaries[@zodiac].generate_5_sentences if zodiacs.include? @zodiac
+  @image = 'https://unsplash.it/g/800/600?blur&random'
+  erb :index
+end
